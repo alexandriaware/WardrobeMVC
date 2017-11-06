20e314bd50a73566f38f6a35d62b88605b546525
alexandriaware <ali.mercer92@gmail.com>
2017-11-05 19:59:47 -0500

    tops and bottoms index view

---
+++ WardrobeMVC/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace WardrobeMVC.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WardrobeMVC.Models.WardrobeDBEntities>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(WardrobeMVC.Models.WardrobeDBEntities context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
